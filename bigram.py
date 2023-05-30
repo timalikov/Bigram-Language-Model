ddf423b724eee4b872d3b20b32a09f01f60962af
timalikov <t_malikov@kbtu.kz>
2023-05-30 17:15:05 +0600

    very start of project

--- bigram.py
+++ bigram.py
@@ -1,11 +1,10 @@
 words = open("names.txt", "r").read().splitlines()
 
 b = {}
-for w in words[:3]:
+for w in words[]:
     chs = ["^"] + list(w) + ["$"]
     for ch1, ch2 in zip(chs, chs[1:]):
         bigram = (ch1, ch2)
         b[bigram] = b.get(bigram, 0) + 1
         print(ch1, ch2)
 
-print(*b)
