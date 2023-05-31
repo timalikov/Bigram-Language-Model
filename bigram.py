2e9c71770e91ff18e31621e1e37530a1340cf38d
timalikov <t_malikov@kbtu.kz>
2023-05-31 20:45:22 +0600

    completed bigram name generation and using neural network training

--- bigram.py
+++ bigram.py
@@ -1,108 +1,205 @@
 import torch
+import torch.nn.functional as F
+
+def read_words_from_file(filename):
+    words = open(filename, "r").read().splitlines()
+    return words
+
+
+
+def compute_bigram_matrix(words):
+    # Initialize the bigram matrix N with zeros
+    N = torch.zeros((27, 27), dtype=torch.int32)
+
+    # Get a sorted list of unique characters from all the words
+    chars = sorted(list(set(''.join(words))))
+    
+    # Create a dictionary mapping each character to its integer representation (index)
+    stoi = {s: i + 1 for i, s in enumerate(chars)}
+    
+    # Add special symbol '^' with integer representation 0
+    stoi['^'] = 0
+    
+    # Create the inverse mapping of stoi to convert integer representation back to character
+    itos = {i: s for s, i in stoi.items()}
+    
+    # Iterate over each word in the list of words
+    for w in words:
+        # Add special symbols '^' at the beginning and end of each word
+        chs = ["^"] + list(w) + ["^"]
+        
+        # Iterate over each pair of characters in the word
+        for ch1, ch2 in zip(chs, chs[1:]):
+            # Get the integer representations of the characters
+            ix1 = stoi[ch1]
+            ix2 = stoi[ch2]
+            
+            # Increment the count in the corresponding cell of the bigram matrix
+            N[ix1, ix2] += 1
+    
+    return N, stoi, itos
+
+
+
+def generate_names(N, stoi, itos):
+    # Initialize the bigram probability matrix P by adding 1 to N and converting to float
+    P = (N + 1).float()
+    
+    # Normalize the rows of P to get probabilities
+    P /= P.sum(1, keepdims=True)
+    
+    # Set the seed for the random generator
+    g = torch.Generator().manual_seed(2147483647)
+    
+    out = []
+    
+    # Generate 5 names
+    for i in range(5):
+        generated_name = []
+        ix = 0
+        
+        # Generate a name until the end symbol '^' is encountered
+        while True:
+            # Get the probability distribution for the next character based on current character
+            p = P[ix]
+            
+            # Sample a character index based on the probability distribution
+            ix = torch.multinomial(p, num_samples=1, replacement=True, generator=g).item()
+            
+            # Convert the character index back to the actual character and append to the generated name
+            generated_name.append(itos[ix])
+            
+            # Check if the end symbol '^' is encountered
+            if ix == 0:
+                break
+        
+        # Add the generated name to the output list
+        out.append(''.join(generated_name))
+    
+    return out
+
+
+
+def compute_likelihood(words, stoi, P):
+    log_likelihood = 0.0
+    n = 0
+    
+    # Compute the log likelihood and count of bigrams
+    for w in words:
+        chs = ['^'] + list(w) + ['^']
+        for ch1, ch2 in zip(chs, chs[1:]):
+            ix1 = stoi[ch1]
+            ix2 = stoi[ch2]
+            
+            # Get the probability of the bigram
+            prob = P[ix1, ix2]
+            
+            # Compute the logarithm of the probability
+            logprob = torch.log(prob)
+            
+            # Accumulate the log likelihood
+            log_likelihood += logprob
+            
+            # Increment the count of bigrams
+            n += 1
+    
+    # Calculate the negative log likelihood and average negative log likelihood
+    nll = -log_likelihood
+    avg_nll = nll / n
+    
+    return log_likelihood.item(), nll.item(), avg_nll.item()
+
+
+#####################################################
+#                   Neural Network                  #
+#####################################################
+
+
+def create_bigram_dataset(words, stoi):
+    xs, ys = [], []
+    for w in words:
+        chs = ['^'] + list(w) + ['^']
+        for ch1, ch2 in zip(chs, chs[1:]):
+            ix1 = stoi[ch1]
+            ix2 = stoi[ch2]
+            xs.append(ix1)
+            ys.append(ix2)
+    
+    xs = torch.tensor(xs)
+    ys = torch.tensor(ys)
+    
+    return xs, ys
+
+
+
+def perform_gradient_descent(xs, ys):
+    num_classes = 27
+    num = xs.nelement()
+    # print('number of examples:', num)
+
+    g = torch.Generator().manual_seed(2147483647)
+    W = torch.randn((num_classes, num_classes), generator=g, requires_grad=True)
+
+    for k in range(1):
+        xenc = F.one_hot(xs, num_classes=num_classes).float()
+        logits = xenc @ W
+        counts = logits.exp()
+        probs = counts / counts.sum(1, keepdims=True)
+        loss = -probs[torch.arange(num), ys].log().mean() + 0.01 * (W ** 2).mean()
+        # print(loss.item())
+
+        W.grad = None
+        loss.backward()
+        W.data += -50 * W.grad
+
+    generated_names = []
+    g = torch.Generator().manual_seed(2147483647)
+    for i in range(5):
+        generated_name = []
+        ix = 0
+        while True:
+            xenc = F.one_hot(torch.tensor([ix]), num_classes=num_classes).float()
+            logits = xenc @ W
+            counts = logits.exp()
+            p = counts / counts.sum(1, keepdims=True)
+            ix = torch.multinomial(p, num_samples=1, replacement=True, generator=g).item()
+            generated_name.append(itos[ix])
+            if ix == 0:
+                break
+        generated_names.append(''.join(generated_name))
+    
+    return generated_names
 
 # Read the words from the file and split them into a list of lines
-words = open("names.txt", "r").read().splitlines()
+words = read_words_from_file("names.txt")
 
-N = torch.zeros((27,27), dtype=torch.int32)     #2D array to store bigrams: rows are for first character, columns are for second
+# Compute the bigram matrix
+N, stoi, itos = compute_bigram_matrix(words)
 
-chars = sorted(list(set(''.join(words))))       #set of all characters that appear in names
-stoi = {s:i+1 for i,s in enumerate(chars)}      #creating integer representation of each character
-stoi['^'] = 0                                   #special symbols for starting and ending points
-itos = {i:s for s,i in stoi.items()}            #Create the inverse mapping of stoi to convert integer representation back to character
+# Generate names using the bigram matrix
+generated_names = generate_names(N, stoi, itos)
 
-for w in words:
-    chs = ["^"] + list(w) + ["^"]       # Add special symbols at the beginning and end of each word
-    for ch1, ch2 in zip(chs, chs[1:]): 
-        # Get the integer representation of the characters
-        ix1 = stoi[ch1]
-        ix2 = stoi[ch2]
-        N[ix1, ix2] += 1                # Increment the count in the corresponding cell of the bigram matrix
+# Compute likelihood of the data
+log_likelihood, nll, avg_nll = compute_likelihood(words, stoi, (N+1).float() / (N+1).sum(1, keepdims=True))
 
+# Create the bigram dataset for the neural network
+xs, ys = create_bigram_dataset(words, stoi)
 
+# Perform gradient descent
+generated_names_nn = perform_gradient_descent(xs, ys)
 
+# Print the generated names from the bigram model
+print("Generated Names (Bigram Model):")
+for name in generated_names:
+    print(name)
 
-######Generator
-P = (N+1).float()               # Initialize the bigram probability matrix P
-P /= P.sum(1, keepdims=True)
-
-g = torch.Generator().manual_seed(2147483647)       # Set the seed for the random generator
-
-out = []
-ix = 0
-for i in range(5):
-  
-  out = []
-  ix = 0
-  while True:
-    p = P[ix]
-    ix = torch.multinomial(p, num_samples=1, replacement=True, generator=g).item()
-    out.append(itos[ix])
-    if ix == 0:
-        break
-  print(''.join(out))
-
-
-###############################################################################
-# Quality Check. GOAL: Maximize likelihood of the data (statistical modeling) #
-# Maximize log likelihood (log is monotonic)                                  #
-# Minimize negative log likelihood (equivalent to maximizing log likelihood)  #
-# Minimize average negative log likelihood                                    #
-# Property: log(a*b*c) = log(a) + log(b) + log(c)                             #
-###############################################################################
-
-
-log_likelihood = 0.0  
-n = 0  # the count of bigrams
-
-for w in words:
-    chs = ['^'] + list(w) + ['^']
-    
-    for ch1, ch2 in zip(chs, chs[1:]):
-        # Get the integer representation of the characters
-        ix1 = stoi[ch1]
-        ix2 = stoi[ch2]
-        
-        # Get the probability of the bigram
-        prob = P[ix1, ix2]
-        
-        # Compute the logarithm of the probability
-        logprob = torch.log(prob)
-        
-        # Accumulate the log likelihood
-        log_likelihood += logprob
-        
-        # Increment the count of bigrams
-        n += 1
-        
+# Print the generated names from the neural network model
+print("Generated Names (Neural Network Model):")
+for name in generated_names_nn:
+    print(name)
 
 # Print the log likelihood, negative log likelihood, and average negative log likelihood
-print(f'{log_likelihood=}')
-nll = -log_likelihood
-print(f'{nll=}')
-print(f'{nll/n}')
+print("Log Likelihood:", log_likelihood)
+print("Negative Log Likelihood:", nll)
+print("Average Negative Log Likelihood:", avg_nll)
 
-
-
-
-##########################################################################################################################
-######################################                                           #########################################
-######################################             Neural Network                #########################################
-######################################                                           #########################################
-##########################################################################################################################
-
-#####################################################
-# Creating the bigram dataset for the Neural Network#
-#####################################################
-# xs, ys = [], []
-
-# for w in words[:1]:
-#     chs = ['.'] + list(w) + ['.']
-#     for ch1, ch2 in zip(chs, chs[1:]):
-#         ix1 = stoi[ch1]
-#         ix2 = stoi[ch2]
-#         # print(ch1, ch2)
-#         xs.append(ix1)
-#         ys.append(ix2)
-    
-# xs = torch.tensor(xs)
-# ys = torch.tensor(ys)
